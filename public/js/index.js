2e92abbf5162fca5a4d0e0ca3bc706d41a8c109b
itorrero98 <issiactorrero@u.boisestate.edu>
2018-11-02 10:38:53 -0600

    Added form for messages and dynamic loading of incoming messages in browser

--- public/js/index.js
+++ public/js/index.js
@@ -10,4 +10,22 @@ socket.on("disconnect", function() {
 
 socket.on("newMessage", function(details) {
   console.log("Got new message ", details);
+
+  var li = jQuery("<li></li>");
+  li.text(`${details.from}: ${details.text}`);
+
+  jQuery("#messages").append(li);
+});
+
+jQuery("#message-form").on("submit", function(e) {
+  e.preventDefault();
+
+  socket.emit(
+    "createMessage",
+    {
+      from: "User",
+      text: jQuery("[name=Message]").val()
+    },
+    function() {}
+  );
 });
--- server/server.js
+++ server/server.js
@@ -34,11 +34,12 @@ io.on("connection", socket => {
     console.log("Client disconnected");
   });
 
-  socket.on("createMessage", newMessage => {
+  socket.on("createMessage", (newMessage, callback) => {
     console.log("User email recieved ", newMessage);
 
     io.emit("newMessage", generateMessage(newMessage.from, newMessage.text));
 
+    callback("This is from the server");
     // socket.broadcast.emit("newMessage", {
     //   from: newMessage.from,
     //   text: newMessage.text,
